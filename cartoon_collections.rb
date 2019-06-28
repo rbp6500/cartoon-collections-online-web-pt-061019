059a08bab906083dda2f90ff5024147852776af2
Rashard Pleasant <rashard.pleasant@gmail.com>
2019-06-28 23:14:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,15 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
-end
+ def roll_call_dwarves ["Doc", "Dopey", "Bashful", "Grumpy"]
+   dwarvers.each_with_index.do |number,name|
+   roll_call_dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+  end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet ["earth", "wind", "fire", "water", "heart"]
+  ["earth", "wind", "fire", "water", "heart"].map (names)
 end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
+'''
+def long_planeteer_calls
+  
+   
 end
 
 def find_the_cheese# code an argument here
